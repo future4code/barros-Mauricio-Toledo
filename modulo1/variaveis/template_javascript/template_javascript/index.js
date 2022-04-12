36fea373932a92b91b5380011f1989ca2baf8772
mlt3800 <mauricioluiz3800@gmail.com>
2022-04-12 16:06:13 -0300

    Desafio

--- modulo1/variaveis/template_javascript/template_javascript/index.js
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -4,20 +4,30 @@ console.log(typeof nome , typeof idade)
 /* Acima aprensenta a ausencia de valores nas variaveis por isso o erro */
 console.log("Ola meu nome é" , nome , "tenho" , idade , "anos")
 
-let a = prompt("você é fã da marvel ?")
-let b = prompt("Você gosta de anime ?")
-let c = prompt("Voce gosta de rock ?")
-// Prompt//
+let a = console.log("você é fã da marvel ?" , "Sim")
+let b = console.log("Você gosta de anime ?" , "Sim")
+let c = console.log("Voce gosta de rock ?" , "Sim")
+// Exercicios/
 
 let x = 40
 let k = 29 
 let m = 31
-console.log(k , m , x)
+
 x = m
 k = x
 m = k
 console.log("o valor de k é " , m)
 console.log("o valor de  m é" , x)
 console.log("o valor de  x é" , k )
+// Desafio//
 
+let z = prompt("escolha um número")
+let y = prompt("Digite um número")
     
+let variavelalpha = (x)
+let variavelbravo = (y)
+
+let soma = variavelalpha + variavelbravo
+let mutiplica = variavelalpha * variavelbravo
+
+console.log(soma , mutiplica)
