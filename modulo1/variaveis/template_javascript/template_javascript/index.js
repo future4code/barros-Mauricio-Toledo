94ac2ea4e66215ff16ca05a4e6050e7b0d344555
mlt3800 <mauricioluiz3800@gmail.com>
2022-04-11 23:45:33 -0300

    aula de variaveis

---
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,23 @@
+const nome = "Mauricio"
+let idade = 31
+console.log(typeof nome , typeof idade)
+/* Acima aprensenta a ausencia de valores nas variaveis por isso o erro */
+console.log("Ola meu nome é" , nome , "tenho" , idade , "anos")
+
+let a = prompt("você é fã da marvel ?")
+let b = prompt("Você gosta de anime ?")
+let c = prompt("Voce gosta de rock ?")
+// Prompt//
+
+let x = 40
+let k = 29 
+let m = 31
+console.log(k , m , x)
+x = m
+k = x
+m = k
+console.log("o valor de k é " , m)
+console.log("o valor de  m é" , x)
+console.log("o valor de  x é" , k )
+
+    
