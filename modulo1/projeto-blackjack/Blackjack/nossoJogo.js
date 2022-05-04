2fecffe4b9ef65069964799541617db9d3823327
mlt3800 <mauricioluiz3800@gmail.com>
2022-05-04 14:02:12 -0300

    Projeto Blackjack e desafios

---
+++ modulo1/projeto-blackjack/Blackjack/nossoJogo.js
@@ -0,0 +1,66 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+/*let jogador = 0
+let Pc = 0
+let primeiraCarta = comprarCarta()
+let segundaCarta = comprarCarta()
+let primeiraCartaPc = comprarCarta()
+let segundaCartaPc = comprarCarta()
+
+let pontuacao = primeiraCarta.valor+segundaCarta.valor+jogador
+let pontuacaoPc = primeiraCartaPc.valor+segundaCartaPc.valor+Pc
+let comecar = confirm("Deseja começar uma partida ?")
+   
+
+
+if (comecar) {console.log(primeiraCarta.texto,segundaCarta.texto,`Total : ${pontuacao}`);
+console.log(primeiraCartaPc.texto,segundaCartaPc.texto,`Total: ${pontuacaoPc}`);
+
+   
+} if (  pontuacao == 21 || pontuacao > pontuacaoPc) {console.log("Parabens você ganhou");
+   
+} if ( pontuacaoPc == 21 || pontuacao < pontuacaoPc) {console.log( "Voce perdeu");
+   
+} else console.log("Fim"); */
+// exercicio comentado para não interferir no desafio.
+   
+
+
+
+
+
+
+
+
+
+  
+
+   
+
+   
+
+ 
+     
+  
+     
+  
+
+
+
+   
+
+
+
+
+
