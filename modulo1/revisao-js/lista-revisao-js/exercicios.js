ec4f9bdef05c2dad663180bdd88501aa87540b61
mlt3800 <mauricioluiz3800@gmail.com>
2022-05-09 22:06:11 -0300

    Exercicio 3

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -16,7 +16,18 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+    function compararOrdem(a,b) {
+        if (a == b) {return 0
+            
+        } if (a < b) { return -1
+            
+        }if (a > b) { return 1
+            
+        }
+        
+    } array.sort(compararOrdem)
+ 
+  return  array.sort(compararOrdem)
 }
 
 // EXERCÍCIO 04
