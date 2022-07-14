362ffa9781ecbe03a196ae24d85cb4edcc1dc7c4
mlt3800 <mauricioluiz3800@gmail.com>
2022-07-14 00:10:11 -0300

    Thinking-react

---
+++ modulo3/thinking-react/e-commerce/src/Components/MockDeDados.js
@@ -0,0 +1,9 @@
+import React from "react";
+export const MockDeDados = [
+  { id: 1, Nome: "Produto 1", Valor: "R$:123,00", imagem: 'https://picsum.photos/id/237/200/300'},
+  { id: 2, Nome: "Produto 2", Valor: "R$:123,00", imagem: 'https://picsum.photos/id/237/200/300'},
+  { id: 3, Nome: "Produto 3", Valor: "R$:123,00", imagem: 'https://picsum.photos/id/237/200/300'},
+  { id: 4, Nome: "Produto 4", Valor: "R$:123,00", imagem: 'https://picsum.photos/id/237/200/300'},
+  { id: 5, Nome: "Produto 5", Valor: "R$:123,00", imagem: 'https://picsum.photos/id/237/200/300'},
+  { id: 6, Nome: "Produto 6", Valor: "R$:123,00", imagem: 'https://picsum.photos/id/237/200/300'},
+];
