4d6ea6c5ed72c1e7db722b94922b3d37e7b61829
mlt3800 <mauricioluiz3800@gmail.com>
2022-08-02 22:56:11 -0300

    versão beta

---
+++ modulo3/astromatch/astromatch/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TelaInicial from './components/TelaInicial';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TelaInicial/>
+    </div>
+  );
+}
+
+export default App;
