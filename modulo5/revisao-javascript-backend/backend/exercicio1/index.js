c7b6d7f6a9fb6d85954e45e2e89196b4aad23bc4
mlt3800 <mauricioluiz3800@gmail.com>
2022-10-04 18:44:27 -0300

    ex1

---
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -0,0 +1,9 @@
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById('clear').onclick = clearForm;
+
+function tempConvert(){
+    let fahrenheit = document.getElementById("fahrenheit")
+}
+
+
+console.log("exercicio 4");
