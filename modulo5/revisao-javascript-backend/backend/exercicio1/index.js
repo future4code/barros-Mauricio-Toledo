d8a3c55da9c229dbfdab43ed9a5e3a27ea447327
mlt3800 <mauricioluiz3800@gmail.com>
2022-10-06 18:02:59 -0300

    ex1 add

--- modulo5/revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,9 +1,20 @@
-document.getElementById('convert').onclick = tempConvert;
-document.getElementById('clear').onclick = clearForm;
+console.log("exercicio 1");
 
-function tempConvert(){
-    let fahrenheit = document.getElementById("fahrenheit")
+
+function converter(Celsius, mesure) {
+    if(mesure === "F" ){
+        farenheit = Celsius*9/5+32
+        return(`${Celsius}° Celsius é = ${farenheit}° Farenheit.`)
+    } if (mesure === "K"){
+        kelvin = Celsius+273.15
+        return(`${Celsius}° Celsius é = ${kelvin}° Kelvin`)
+    } if (mesure !== "F" && mesure !== "K"){
+        return (`Error, valor errado ${mesure}`)
+    }if( typeof Number === Celsius ){
+        return(`Erorr. wrong one ${Celsius}`)
+    }
 }
 
-
-console.log("exercicio 4");
+console.log(conversor(30, "F"))
+console.log(conversor(40, "K"))
+console.log(conversor(40,"j"))
