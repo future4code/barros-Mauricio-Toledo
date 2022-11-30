716ca39c67ae6d09677de9f8a29c1a04d5ca3379
Mauricio Luiz de toledo <mauricioluiz3800@gmail.com>
2022-11-30 15:43:53 -0300

    Pt3

--- modulo5/revisao-mod5-backend/Revisao/src/index.ts
+++ modulo5/revisao-mod5-backend/Revisao/src/index.ts
@@ -1,8 +1,17 @@
-import chalk from "chalk"
+// Parte 1 e 2
+/*import chalk from "chalk"
 
 let message: string|number = chalk.blue("Ola mundo,hello world!");
 
  message = 42
 
 
-console.log(message);
+console.log(message);*/
+// Parte 3
+
+import app from "./app"
+import getAllCharacters from "./endpoints/getAllCharacters"
+import createCharacter from './endpoints/createCharacters';
+
+app.get("/character", getAllCharacters)
+app.put("/character", createCharacter)
