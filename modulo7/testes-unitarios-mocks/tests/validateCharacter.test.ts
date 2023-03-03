5322e6765197ac9291c731c320e4823c824d3034
Mauricio Luiz de toledo <102395706+mlt3800@users.noreply.github.com>
2023-03-03 10:11:58 +0000

    ex2

--- modulo7/testes-unitarios-mocks/tests/validateCharacter.test.ts
+++ modulo7/testes-unitarios-mocks/tests/validateCharacter.test.ts
@@ -1,16 +1,69 @@
-import { validateCharacter } from "../src/functions/validateCharacter";
-import { character } from '../src/models/character';
+import { validateCharacter } from "../src/functions/validateCharacter"
+import { character } from "../src/models/character"
 
-describe("Testing the function validadeCharacter", () => {
-    test("Should receive a character with the property name empty and return false", () => {
+describe("Testando validadeCharacter", () => {
+    test("Recebe a via "" e retorna falso", () => {
         const character: character = {
             name: "",
-            life: 20,
-            defense: 100,
-            strength: 50
+            life: 200,
+            defense: 50,
+            strength: 5000
         }
 
         const result = validateCharacter(character)
 
         expect(result).toBe(false)
-   } )})
+    })
+
+    test("Recebe vida = 0 e retorna falso", () => {
+        const character: character = {
+            name: "Carol",
+            life: 0,
+            defense: 200,
+            strength: 100
+        }
+
+        const result = validateCharacter(character)
+
+        expect(result).toBe(false)
+    })
+
+    test("Recebe força = 0  retorna false", () => {
+        const character: character = {
+            name: "Eleanor",
+            life: 55,
+            defense: 150,
+            strength: 0
+        }
+
+        const result = validateCharacter(character)
+
+        expect(result).toBe(false)
+    })
+
+    test("Recebe valor negativo da força e defesa e retorna falso", () => {
+        const character: character = {
+            name: "Esdras",
+            life: 220,
+            defense: -30,
+            strength: -8
+        }
+
+        const result = validateCharacter(character)
+
+        expect(result).toBe(false)
+    })
+
+    test("Recebe as informações e retona true", () => {
+        const character: character = {
+            name: "Bobby",
+            life: 200,
+            defense: 150,
+            strength: 250
+        }
+
+        const result = validateCharacter(character)
+
+        expect(result).toBe(true)
+    })
+})
